d4a4189b28068d602f44ab6274c9ddc84785747a
stephane <stephane@stephane-W54xEU.(none)>
2013-04-21 19:48:19 +0200

    reddit image downloader

--- reddit_img_grabber.go
+++ reddit_img_grabber.go
@@ -5,6 +5,11 @@ import (
   "net/http"
   "io/ioutil"
   "encoding/json"
+  "image"
+  "image/png"
+  _ "image/jpeg"
+  "os"
+  "strings"
 )
 
  
@@ -12,7 +17,6 @@ import (
 type JsonResponse struct {
   Kind string
   Data DataType
-  //Databases []DatabasesType
 }
  
 type DataType struct {
@@ -29,18 +33,50 @@ type DataType2 struct {
 
  
 func main() {
-  response, _ := http.Get("http://reddit.com/r/hot.json")
+ urls := geturls("hot")
+ for _, url := range urls {
+    saveimage(url)
+  }
+}
+
+
+func geturls(subreddit string) []string  {
+  response, _ := http.Get("http://reddit.com/r/" + subreddit + ".json")
   defer response.Body.Close()
   contents, _ := ioutil.ReadAll(response.Body)
   var resp JsonResponse
   err := json.Unmarshal(contents, &resp)
   if err != nil {
-    fmt.Println(err)
+    fmt.Println("shit happened")
   }
   children := resp.Data.Children
   var urls []string 
   for _, val := range children {
     urls = append(urls, val.Data.Url)
   }
-  fmt.Println(urls)
+  return urls
+}
+
+func urltoname(url string) string {
+  split := strings.Split(url, "/")
+  lenofsplit := len(split)
+  indexoflast := lenofsplit - 1
+  namewithext := split[indexoflast]
+  split2 := strings.Split(namewithext, ".")
+  name := split2[0]
+  return name
+}
+
+func saveimage(url string) {
+  name := urltoname(url)
+  response_img, _ := http.Get(url)
+  img, _, _ := image.Decode(response_img.Body)
+  if img != nil {
+    file, _ := os.Create("/home/stephane/imgs/" + name + ".png")
+    err := png.Encode(file, img)
+    if err != nil {
+      fmt.Println(err)
+    }
+    file.Close()
+  }
 }
