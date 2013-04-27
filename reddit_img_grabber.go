e3d51f30bfd868e0bf5194efa894d4d60d320ea8
Stephane <stehem@gmail.com>
2013-04-27 13:41:53 +0200

    add concurrent downloading :-)

--- reddit_img_grabber.go
+++ reddit_img_grabber.go
@@ -1,5 +1,7 @@
+// concurrently download images from any subreddit
+
 package main
- 
+
 import (
   "fmt"
   "net/http"
@@ -12,13 +14,12 @@ import (
   "strings"
 )
 
- 
 
 type JsonResponse struct {
   Kind string
   Data DataType
 }
- 
+
 type DataType struct {
   Children []ChildrenType
 }
@@ -31,14 +32,39 @@ type DataType2 struct {
   Url string
 }
 
- 
-func main() {
- urls := geturls("hot")
- for _, url := range urls {
-    saveimage(url)
-  }
+
+type RedditImage struct {
+  url string
+  name string
+  http_resp *http.Response
 }
 
+
+func main() {
+  var subreddit directory string
+  fmt.Print("Subreddit: ")
+  fmt.Scanln(&subreddit)
+  fmt.Print("Path where to save: ")
+  fmt.Scanln(&directory)
+  grab(subreddit, directory)
+}
+
+func grab(sub string, dir string) {
+  ch := make(chan RedditImage)
+  urls := geturls(sub)
+
+  for _, url := range urls {
+    reddit := RedditImage{url: url, name: urltoname(url)}
+    go fetchimage(reddit, ch)
+  }
+
+  for {
+    select {
+      case redditimg := <-ch:
+        saveimage(redditimg, dir)
+    }
+  }
+}
 
 func geturls(subreddit string) []string  {
   response, _ := http.Get("http://reddit.com/r/" + subreddit + ".json")
@@ -50,7 +76,7 @@ func geturls(subreddit string) []string  {
     fmt.Println("shit happened")
   }
   children := resp.Data.Children
-  var urls []string 
+  var urls []string
   for _, val := range children {
     urls = append(urls, val.Data.Url)
   }
@@ -67,12 +93,17 @@ func urltoname(url string) string {
   return name
 }
 
-func saveimage(url string) {
-  name := urltoname(url)
-  response_img, _ := http.Get(url)
-  img, _, _ := image.Decode(response_img.Body)
+func fetchimage(redditimg RedditImage, ch chan RedditImage) {
+  resp, _ := http.Get(redditimg.url)
+  redditimg.http_resp = resp
+  ch <- redditimg
+}
+
+func saveimage(redditimg RedditImage, dir string) {
+  name := redditimg.name
+  img, _, _ := image.Decode(redditimg.http_resp.Body)
   if img != nil {
-    file, _ := os.Create("/home/stephane/imgs/" + name + ".png")
+    file, _ := os.Create(dir + name + ".png")
     err := png.Encode(file, img)
     if err != nil {
       fmt.Println(err)
@@ -80,3 +111,6 @@ func saveimage(url string) {
     file.Close()
   }
 }
+
+
+
